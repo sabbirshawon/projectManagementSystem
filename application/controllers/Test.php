354e77f2656f54d2e7c1df7e15334baa3e8fa0a6
nuruliislam <mdnrl59@gmail.com>
2020-10-17 13:51:26 +0100

    adding Test controller for testing

---
+++ application/controllers/Test.php
@@ -0,0 +1,12 @@
+<?php 
+   class Test extends CI_Controller {
+  
+      public function index() { 
+         echo "Hello World!"; 
+      }
+      
+      public function hello() { 
+        echo "This is hello function."; 
+     } 
+   } 
+?>

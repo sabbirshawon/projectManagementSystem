0748ae09594342bc53e5df6e92b98d5affee84bf
nuruliislam <mdnrl59@gmail.com>
2020-10-23 00:54:55 +0100

    modified sub-header to access Department controller

--- application/views/backend/sub-header.php
+++ application/views/backend/sub-header.php
@@ -5,12 +5,12 @@
               <div class="inner">
                 <h3>150</h3>
 
-                <p>New Orders</p>
+                <p>Department</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="<?php echo base_url();?>Department" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->

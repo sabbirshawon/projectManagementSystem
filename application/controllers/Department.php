7943cc0d56f0c32e53d5c1a03bdbc2ec90075a44
nuruliislam <mdnrl59@gmail.com>
2020-10-23 00:55:34 +0100

    adding Department controller

---
+++ application/controllers/Department.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Department extends CI_Controller{
+    function __construct()
+    {
+        parent:: __construct();
+    }
+  
+    public function index(){
+        //print_r('Department');
+        $this->load->view('backend/layouts/header');
+        $this->load->view('backend/department_view');
+        $this->load->view('backend/layouts/footer');
+    }
+}
+?>

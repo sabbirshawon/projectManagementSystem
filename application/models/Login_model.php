239dd71ce4335d7cca509c31ce0654d038018ab5
nuruliislam <mdnrl59@gmail.com>
2020-10-22 10:14:55 +0100

    adding login model

---
+++ application/models/Login_model.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login_model extends CI_Model
+{
+     function __construct()
+     {
+          // Call the Model constructor
+          parent::__construct();
+     }
+
+     function login($email, $password){
+        //print_r($email." ". $password);
+
+        
+     }
+
+}
+?>

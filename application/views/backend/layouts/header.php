c5b696e45e2deb8ce7f9036036839a66ef9fdc23
nuruliislam <mdnrl59@gmail.com>
2020-10-22 09:55:38 +0100

    modified header file

--- application/views/backend/layouts/header.php
+++ application/views/backend/layouts/header.php
@@ -170,7 +170,7 @@
           <img src="<?php echo base_url();?>assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">Alexander Pierce</a>
         </div>
       </div>
 

5843734a2b432df455c4f8446fad9b83660eb013
nuruliislam <mdnrl59@gmail.com>
2020-10-22 02:56:05 +0100

    modified login.php to run Dashboard controller from login button

--- application/views/frontend/login.php
+++ application/views/frontend/login.php
@@ -28,7 +28,7 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="#" method="post">
+      <form action="<?php echo base_url();?>Dashboard" method="post">
         <div class="input-group mb-3">
           <input type="email" class="form-control" placeholder="Email">
           <div class="input-group-append">

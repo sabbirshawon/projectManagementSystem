21fa0d32c8debb581c843cc77f6c44e35aacd8ff
nuruliislam <mdnrl59@gmail.com>
2020-10-23 12:34:17 +0100

    modified Test controller

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,12 +1,18 @@
-<?php 
-   class Test extends CI_Controller {
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+   class Projects extends CI_Controller{
+      function __construct()
+      {
+         parent:: __construct();
+      }
   
       public function index() { 
          $this->load->view('frontend/login'); 
       }
-      
+
       public function hello() { 
-        echo "This is hello function."; 
-     } 
-   } 
+         echo "This is hello function."; 
+      } 
+}
 ?>

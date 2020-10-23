843ea7a4824e8d89a0d2d4796685f972050541a1
nuruliislam <mdnrl59@gmail.com>
2020-10-23 04:22:49 +0100

    modified department view to add modal

--- application/views/backend/department_view.php
+++ application/views/backend/department_view.php
@@ -122,7 +122,49 @@
             <!-- /.card -->
 
 
+            <!-- modal for add department -->
+            <div class="modal fade" id="addDesg" tabindex="-1" role="dialog" aria-labelledby="addDesignation" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Add New Designation</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="<?php echo base_url();?>department/add_designation" method="post">
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Department Name:</label>
+                                    <?php echo form_dropdown('deptID',$deptt_info,'',' class="form-control" id="dept_name"');?>
+                                </div>
 
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Designation Name:</label>
+                                    <input type="text" class="form-control" id="recipient-name" name="designationName" required="required">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="message-text" class="col-form-label">Designation Status:</label>
+                                    <select name="desgStatus" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                            
+                        
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+
+
+            <!-- modal for add Designation -->
             <div class="modal fade" id="addDepat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -155,7 +197,9 @@
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div>
+
+
 
             
           </div>

25d6e36d83e49acdb92f7d91ab7aad9c98ac6520
nuruliislam <mdnrl59@gmail.com>
2020-10-23 12:35:21 +0100

    modified sub-header

--- application/views/backend/sub-header.php
+++ application/views/backend/sub-header.php
@@ -19,15 +19,12 @@
             <div class="small-box bg-success">
               <div class="inner">
                 <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                <p>Bounce Rate</p>
-                
-                
+                <p>View Projects</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="<?php echo base_url();?>Projects" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->

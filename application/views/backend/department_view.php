d1556311f91d632a5f5c7c7a7836c5a08aaec402
nuruliislam <mdnrl59@gmail.com>
2020-10-23 01:24:37 +0100

    adding modal in department_view

--- application/views/backend/department_view.php
+++ application/views/backend/department_view.php
@@ -27,7 +27,7 @@
                 <h3 class="card-title">DataTable with default features</h3>
                 <br>
                 <br>
-                <a type="button" class="btn btn-primary">Add New Department</a>
+                <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#addDepat" data-whatever="@mdo">Add New Department</a>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -56,6 +56,39 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+
+            <div class="modal fade" id="addDepat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Add New Department</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="#" method="post">
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Department Name:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="message-text" class="col-form-label">Department Status:</label>
+                                    <select name="deptStatus" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Send message</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
           </div>
           <!-- /.col -->
         </div>

43c9253cf858c536d449e37d017d4b00d253ec8d
nuruliislam <mdnrl59@gmail.com>
2020-10-24 13:57:14 +0100

    updated Products controller to add products

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -8,9 +8,17 @@ class Product extends CI_Controller{
     }
   
     public function index() { 
+
+        $data['title'] = "Product";
+        $data['cat_info'] = $this->products_model->get_category();
         $this->load->view('backend/layouts/header');
-        $this->load->view('backend/products_view');
+        $this->load->view('backend/products_view',$data);
         $this->load->view('backend/layouts/footer');
+    }
+
+    function add_prod(){
+        $this->products_model->add_prod();
+        redirect('product');
     }
 }
 ?>

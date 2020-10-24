ed32857bd5bbcf622d6b0ddc9596b55b34083ca1
nuruliislam <mdnrl59@gmail.com>
2020-10-24 07:30:52 +0100

    modified header

--- application/views/backend/layouts/header.php
+++ application/views/backend/layouts/header.php
@@ -156,7 +156,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="<?php echo base_url();?>Dashboard" class="brand-link">
       <img src="<?php echo base_url();?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">PMS</span>

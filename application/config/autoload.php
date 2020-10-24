899162e8ae0f260178c2e19a1f973fc3c1fd8aa6
nuruliislam <mdnrl59@gmail.com>
2020-10-24 13:56:02 +0100

    updated autoload to add product model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('login_model','department_model','category_model');
+$autoload['model'] = array('login_model','department_model','category_model','products_model');

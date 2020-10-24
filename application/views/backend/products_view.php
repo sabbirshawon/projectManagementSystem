36f727f24c08ebc4337dccf48a1f9d67d5045357
nuruliislam <mdnrl59@gmail.com>
2020-10-24 13:58:25 +0100

    updated products view to add products

--- application/views/backend/products_view.php
+++ application/views/backend/products_view.php
@@ -5,7 +5,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Employee</h1>
+            <h1>Products</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -24,10 +24,10 @@
           <div class="col-12">
             <div class="card" id="department_card">
               <div class="card-header">
-                <h3 class="card-title">Employee</h3>
+                <h3 class="card-title">Products</h3>
                 <br>
                 <br>
-                <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#addEmploy" data-whatever="@mdo">Add New Employee</a>
+                <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#addProd" data-whatever="@mdo">Add New Product</a>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -56,6 +56,47 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+
+            <!-- modal for add Products -->
+            <div class="modal fade" id="addProd" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Add New Category</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="<?php echo base_url();?>product/add_prod" method="post">
+                                <div class="form-group">
+                                    <label class="col-form-label">Category Title: </label>
+                                    <?php echo form_dropdown('category_id',$cat_info,'',' class="form-control"');?>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-form-label">Product Title</label>
+                                    <input type="text" class="form-control" name="product_title" required="required">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-form-label">Category Status: </label>
+                                    <select name="product_status" class="form-control" required="required">
+                                      <option value="1">Active</option>
+                                      <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                            
+                        
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             </div>
           <!-- /.col -->

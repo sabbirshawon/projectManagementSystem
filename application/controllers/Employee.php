3b0e9765fb71a65e937f4243950aa2f599b959af
nuruliislam <mdnrl59@gmail.com>
2020-10-24 07:27:01 +0100

    modified Employee controller

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -8,8 +8,13 @@ class Employee extends CI_Controller{
     }
   
     public function index() { 
+
+        $data['title'] = "Employee";
+        $data['deptt_info'] = $this->department_model->get_deptt();
+        $data['desg_info'] = $this->department_model->get_designation();
+
         $this->load->view('backend/layouts/header');
-        $this->load->view('backend/employee_view');
+        $this->load->view('backend/employee_view',$data);
         $this->load->view('backend/layouts/footer');
     }
 }

ca80d567e85caf357e329ce0b302444ce423bc95
nuruliislam <mdnrl59@gmail.com>
2020-10-25 10:59:11 +0000

    modified header

--- application/views/backend/layouts/header.php
+++ application/views/backend/layouts/header.php
@@ -188,25 +188,29 @@
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
+           </ul>
+          </li>
+
+
+          <li class="nav-item has-treeview menu-open">
+            <a href="<?php echo base_url();?>Attendence" class="nav-link active">
+              <i class="nav-icon fas fa-clock"></i>
+              <p>
+                Attendence
+            
+              </p>
+            </a>
+          </li>
+
+
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fa fa-files-o"></i>
+              <p>
+                Projects
+                
+              </p>
+            </a>
           </li>
          
          

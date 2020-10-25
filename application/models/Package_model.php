1657d76b841a5a9df169eef3b8032b75e5821efa
nuruliislam <mdnrl59@gmail.com>
2020-10-25 09:18:13 +0000

    add package model

---
+++ application/models/Package_model.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	class Package_model extends CI_Model {
+		function __construct(){
+          // Call the Model constructor
+          parent::__construct();
+        }
+
+        function add_package()
+        {
+		
+			date_default_timezone_set('Asia/Dhaka');
+			$date = date('Y-m-d',time());
+			
+			$insert_data= array(
+					'product_id' => $this->input->post('product_id'),
+					'package_title' => $this->input->post('package_title'),
+					'package_status' => $this->input->post('package_status'),
+					'package_doc' => $date,
+					'package_created_by' => $this->session->userdata('user_id')
+						);
+			if($this->db->insert('tbl_package_info',$insert_data)){
+				$data['status'] = 'success';
+			}
+			else{
+				$data['status'] = 'error';
+			}
+		}
+		
+	}
+
+?>

aab2673ba1e7a17c69e1678db02076c3400ce4a8
nuruliislam <mdnrl59@gmail.com>
2020-11-08 08:54:02 +0000

    modifie dept. view to edit designation

--- application/views/backend/department_view.php
+++ application/views/backend/department_view.php
@@ -75,7 +75,7 @@
             <!-- /.card -->
 
 
-            <div class="card" id="department_card">
+            <div class="card" id="designation_card">
               <div class="card-header">
                 <h3 class="card-title">Designation</h3>
                 <br>
@@ -84,6 +84,7 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body">
+              <input type="hidden" id="designation_info_details" value="<?php echo base_url();?>department/get_desg_info_for_edit">
                 <table id="example2" class="table table-bordered table-striped">
                   <thead>
                   <tr>
@@ -94,9 +95,10 @@
                         <th>Action</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody id="desg_edited">
                     <?php foreach ($designation_info as $d_info):?>
                         <tr> 
+                        <input type="hidden" id="desg_<?php echo $d_info['designationID'];?>" value="<?php echo $d_info['designationID'];?>">
                             <td><?php echo $d_info['designationID'];?></td>
                             <td><?php echo $d_info['deptName'];?></td>
                             <td><?php echo $d_info['designationName'];?></td>
@@ -112,7 +114,8 @@
                             </td>
 
                             <td>
-                                <a type="button" class="btn btn-primary">Edit</a>
+                            <input type="hidden" name="designationID" value="<?php echo $d_info['designationID'];?>" style="display: none;">
+                              <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#editDesg" data-whatever="@mdo">Edit</a>
                             </td> 
                         </tr>
                     <?php endforeach;?>
@@ -241,6 +244,42 @@
                     </div>
                 </div>
             </div>
+
+
+            <!-- modal for edit designment -->
+            <div class="modal fade" id="editDesg" tabindex="-1" role="dialog" aria-labelledby="addDesignation" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Update Designation Info</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                          <form  action="<?php echo base_url();?>department/edit_desg" method="post">
+                            <div class="form-group">
+                              <label class="col-form-label">Department Name</label>
+                              <?php echo form_dropdown('deptID',$deptt_info,'',' class="form-control" id="dept_name2"');?>
+                            </div>
+
+                           
+                            <div class="form-group">
+                              <label  class="col-form-label">Designation Name</label>
+                                <input type="text" class="form-control" name="designationName" id="designationName_id" required="required">
+                                <input type="hidden" class="form-control" name="designationID" id="designationID2">
+                            </div>
+                            
+                        
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                          </form>
+                        </div>
+                    </div>
+                </div>
+            </div>    
 
 
 

7a5aea581a139f43d1ed7783d81387b820f6ee24
nuruliislam <mdnrl59@gmail.com>
2020-11-14 01:09:36 +0000

    modified update functionality at catagory view,model,controller

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -35,7 +35,35 @@
             return $data;
         }
 
-        
+        function get_cat_for_edit($category_id){
+			$query= $this->db->select('*')
+							->from('tbl_category_info')
+							->where('tbl_category_info.category_id',$category_id)
+							->get();
+			$data = $query->row_array();
+			return $data;
+		}
+
+
+		function edit_cat(){
+
+			date_default_timezone_set('Asia/Dhaka');
+			$date = date('Y-m-d',time());
+
+			$category_id = $this->input->post('category_id');
+			$data = array(
+				'category_title' => $this->input->post('category_title'),
+				'category_status' => $this->input->post('category_status'),
+				'category_created_by' => $this->session->userdata('user_id')
+				);
+
+			$this->db->where('category_id', $category_id);
+
+			$result = $this->db->update('tbl_category_info', $data);
+
+			return $result;
+			
+		}
     }
 
 ?>
